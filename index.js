c07a2e6b4ba1113e9cde7ecc70a47e4983e68222
SR200802 <66560069+SR200802@users.noreply.github.com>
2021-03-03 00:35:24 +0000

    v1

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const PREFIX = "!";
+const prefix = '!';
+
+const token = 'ODE2NDYxMTI1OTMxNDk5NTMw.YD7SrQ.6yAPehwjgGfqxsp3fcFq0RNDSmI'
+
+client.on('ready', () =>{
+    console.log('This bot is online!, Change Successful');
+    
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'society'){
+        message.channel.send('https://tenor.com/view/society-historyoftheentireworld-gif-8631156');
+    }
+    else if(command === 'hunka hunka')
+    {
+        message.channel.send('https://gfycat.com/frayednextfugu');
+    }
+
+
+
+
+    /*switch (args[0]){
+        case 'duke':
+            client.commands.get('play').execute(messgae, args);
+            break;
+        case 'insult':
+
+            break;
+        case 'testing':
+            message.reply('yup');
+            break;
+        
+
+            
+
+
+        
+            
+
+    }*/
+})
+
+client.login(/*process.env.token*/token);
